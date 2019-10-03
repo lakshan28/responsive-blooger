d15310104795fe076e1b0eb80140039ab4b5c802
lakshan28 <lakshansampath28@gmail.com>
2019-10-03 20:14:41 +0530

    site content

--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
   $toggleCollapse.click(function() {
     $nav.toggleClass("collapse");
   });
+
+  //owl-carousel for blog
+  $(".owl-carousel ").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next")
+    ]
+  });
 });
