66e5794437844042762c75070d2017117b612d56
lakshan28 <lakshansampath28@gmail.com>
2019-10-04 14:25:28 +0530

    page-down-animation(AOS)

--- js/main.js
+++ js/main.js
@@ -18,7 +18,26 @@ $(document).ready(function() {
     navText: [
       $(".owl-navigation .owl-nav-prev"),
       $(".owl-navigation .owl-nav-next")
-    ]
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      320: {
+        items: 1,
+        nav: false
+      },
+      560: {
+        items: 2,
+        nav: true
+      },
+      960: {
+        items: 3,
+        nav: true
+      }
+    }
   });
 
   // click to scroll top
@@ -31,4 +50,7 @@ $(document).ready(function() {
       2000
     );
   });
+
+  //Aos instance
+  AOS.init();
 });
